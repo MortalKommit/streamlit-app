b4661d0677544c6800401887de0b3b75e511c4e2
Kedar K <kedzko@gmail.com>
2023-07-30 22:15:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("Healthy New Diner!")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Plain Omlette")
+streamlit.text("Hardboiled Eggs")
+streamlit.text("Oats")
