db62522576b873bb34b9ba5a3e564af86dbad2d6
Kedar K <kedzko@gmail.com>
2023-08-11 18:30:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,35 +6,35 @@ import snowflake.connector
 
 streamlit.title("Healthy New Diner!")
 
-# streamlit.header('Breakfast Menu')
-# streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
-# streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-# streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
-# streamlit.text(' 🥑 Avocado Toast')
+streamlit.header('Breakfast Menu')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑 Avocado Toast')
 
-# my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-# my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-# fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
-# streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 
-# # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# # fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# # # Tabular data
-# # streamlit.dataframe(fruityvice_normalized)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# Tabular data
+streamlit.dataframe(fruityvice_normalized)
 
-# # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-# # streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-# # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# # fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# # # Tabular data
-# # streamlit.dataframe(fruityvice_normalized)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# Tabular data
+streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
